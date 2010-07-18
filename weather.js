74179d533bd4b167ddd88c59ecf7145b69a25776
Joshua Holbrook <josh.holbrook@gmail.com>
2010-07-18 15:55:29 -0800

    minor error handling.

--- weather.js
+++ weather.js
@@ -3,13 +3,19 @@ var YQL = require('yql');
 
 exports.getWeather = function getWeather (location, callback) {
     new YQL.exec("select * from weather.woeid where w in (select woeid from geo.places where text=@location limit 1)", function(response) {
-        var location = response.query.results.rss.channel.location;
-        var condition = response.query.results.rss.channel.item.condition
-        var units = response.query.results.rss.channel.units
-        var forecast = response.query.results.rss.channel.item.forecast
-        callback(location.city+", "+location.region+":");
-        callback("Now: "+condition.text+", "+condition.temp+" ("+units.temperature+")");
-        callback("Today: "+forecast[0].text+", "+forecast[0].high+"/"+forecast[0].low+" ("+units.temperature+")")
-        callback("Tomorrow: "+forecast[1].text+", "+forecast[1].high+"/"+forecast[1].low+" ("+units.temperature+")")
-        },{"location": location, "env": "store://datatables.org/alltableswithkeys"}); 
+        //TODO: Handlers for less expected errors
+        if (response.query.results.rss.channel.item.title === "City not found") {
+            callback("Error: "+response.query.results.rss.channel.item.title);
+        } else {
+            var location = response.query.results.rss.channel.location;
+            var condition = response.query.results.rss.channel.item.condition
+            var units = response.query.results.rss.channel.units
+            var forecast = response.query.results.rss.channel.item.forecast
+            //Some locations have a blank region. TODO: Look for this.
+            callback(location.city+", "+location.region+":");
+            callback("Now: "+condition.text+", "+condition.temp+" ("+units.temperature+")");
+            callback("Today: "+forecast[0].text+", "+forecast[0].high+"/"+forecast[0].low+" ("+units.temperature+")")
+            callback("Tomorrow: "+forecast[1].text+", "+forecast[1].high+"/"+forecast[1].low+" ("+units.temperature+")")
+        }
+    },{"location": location, "env": "store://datatables.org/alltableswithkeys"}); 
 }
