bb798228d2cd12776542b3a19032ff1dd637fb6d
Joshua Holbrook <josh.holbrook@gmail.com>
2010-07-20 08:19:52 -0800

    forgot a paren :(

--- trackBranch.js
+++ trackBranch.js
@@ -24,7 +24,7 @@ exports.trackBranch = function trackBranch(username, repo, branch, callback) {
             if (commitlist.length > maxcommitlist) {
                 commitlist.push(commitlist.slice(0,Math.floor(maxcommitlist/2)));
                 commitlist.push("...");
-                commitlist.push(commitlist.slice(commitlist.length-Math.floor(maxcommitlist/2),commitlist.length);
+                commitlist.push(commitlist.slice(commitlist.length-Math.floor(maxcommitlist/2),commitlist.length));
                 commitlist.push("(And more! This list was truncated for brevity's sake.)");
             }
             for (i in commitlist) {
