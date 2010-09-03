4fadb9265daa2a52cb065475d51aa0fb73df361c
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-03 12:21:07 -0800

    Merged changes that I made in nStore, which actually had nothing to do with data persistence despite where I did them. shoulda cherry-picked, though, as I grabbed some of the nStore baggage that I didnt want.

--- modifyStore.js
+++
@@ -1,9 +0,0 @@
-var configfile = './gitwatch.db';
-var sys = require('sys');
-var nStore = require('nStore');
-var repl = require('repl');
-
-//global so that the repl may access it.
-watchdb = nStore(configfile);
-
-repl.start();
