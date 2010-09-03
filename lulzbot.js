5f08af665693a4786f536788b8ddd306d147648b
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-03 10:50:51 -0800

    lulzbot now runs its own server

--- lulzbot.js
+++ lulzbot.js
@@ -3,17 +3,35 @@
 var sys = require('sys');
 var DNode = require('dnode');
 var IRC = require('./lib/node-irc/lib/irc');
+var spawn = require('child_process').spawn;
 
 var server = 'irc.freenode.net';
 var nick = 'lulzbot-X';
 var options = { userName: 'lulzbot',
                 realName: 'LulzBot the node.js IRC bot!',
-                debug: false,
+                debug: true,
                 channels: ['#stackvm'],
                 retryCount: 5 };
 
 var irc = new IRC.Client(server, nick, options);
 
+//Some logics that run the server as a separate process
+var server = function () {
+    var spawned = spawn("node", ["services.js"]);
+    spawned.stdout.on("data", function (data) {
+        console.log("Server data: "+data);
+    });
+    spawned.stderr.on("data", function (data) {
+        console.log("Server error: "+data);
+    });
+    spawned.on('exit', function (code) {
+        console.log("Server exited with status "+code);
+        console.log("Restarting.");
+        server();
+    });
+}
+
+//Using dnode to call services
 DNode(function(services) {
     this.triggers = function () {
         irc.on('message', function (from, to, message) {
@@ -29,3 +47,6 @@ DNode(function(services) {
     };
 
 }).listen(12321);
+
+//Runs the server
+server();
