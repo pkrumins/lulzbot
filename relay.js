3edfaeced305148ceaa1b741991b85104e20f336
James Halliday <substack@gmail.com>
2010-09-10 01:20:20 -0800

    cleaner relay with option parsing

--- relay.js
+++ relay.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
+var argv = require('optimist')
+    .usage('Usage: $0 OPTIONS\n'
+        + '    Required OPTIONS: --server --nick\n'
+        + '    Other OPTIONS: --username --realname --channel')
+    .demand([ 'server', 'nick' ])
+    .argv;
 
-//"Server," which runs the IRC side of things
-
-var sys = require('sys');
-
-var DNode = require('dnode');
-
-var IRC = require('./lib/node-irc/lib/irc');
 var spawn = require('child_process').spawn;
 var fs = require('fs');
 
-console.log("Starting irc relay...");
+var DNode = require('dnode');
+var IRC = require('irc');
 
-var ircserver = 'irc.freenode.net';
-var nick = 'lulzbot-X';
-var options = { userName: 'lulzbot',
-                realName: 'LulzBot the node.js IRC bot!',
-                debug: false,
-                channels: ['#stackvm'],
-                retryCount: 5 };
-
-var irc = new IRC.Client(ircserver, nick, options);
+var irc = new IRC.Client(argv.server, argv.nick, {
+    userName : argv.username || argv.nick,
+    realName : argv.realname || 'LulzBot the node.js IRC bot!',
+    debug: false,
+    channels: Array.isArray(argv.channel) ? argv.channel : [argv.channel],
+    retryCount: 5,
+});
 
 function say (to, reply) {
     console.log("Msg to "+to+": "+reply);
