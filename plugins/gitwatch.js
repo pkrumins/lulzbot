9dd76db2c811c892edb9fe8a8aee9fa635864312
James Halliday <substack@gmail.com>
2010-09-10 20:02:51 -0800

    watch and unwatch updates the db correctly now

--- plugins/gitwatch.js
+++ plugins/gitwatch.js
@@ -4,64 +4,44 @@ var sys = require('sys');
 var Hash = require('traverse/hash');
 
 var nStore = require('nStore');
-var watchlist = {};
-
-exports.gitwatch = function (cb) { 
-    var branches = new Branches(
-        nStore(__dirname + '/gitwatch/gitwatch.db')
-    );
-    
-    setInterval(function () {
-        var hacktivity = {};
-        branches.getUpdated(function (err, branch, commit) {
-            if (err) { console.log('Error: %s', err); return }
-            
-            if (!hactivity[branch.key]) {
-                hacktivity[branch.key] = { branch : branch, commits : [] };
-            }
-            hacktivity[key].commits.push(commit);
-        });
-        
-        Hash(hacktivity).forEach(function (repo) {
-            repo.channels.forEach(function (channel) {
-                prepareMessage(repo, function (msg) {
-                    cb(channel, msg);
-                });
-            });
-        });
-    }, 30000);
-};
-
-function prepareMessage (repo, cb) {
-    var greetz = ["Whoa Nelly!", "Zounds!", "Egads!", "Oh snap!", "Aack!"];
-    var greet = greetz[ Math.floor(Math.random() * greetz.length) ];
-    
-    var b = repo.branch;
-    var name = b.user + '/' + b.repo + '/ (' + b.name + ')';
-    
-    var commits = repo.commits;
-    
-    cb(greet + ' ' + commits.length + ' new commits to ' + name + '!');
-    
-    commits.slice(0,4).forEach(function (commit) {
-        cb('    * ' + commit.author.name + ': ' + commit.message);
-    });
-    
-    var more = commits.length - 4;
-    if (commits.length > 4) cb('    ... and ' + more + ' more!');
-}
+module.exports = new Branches(nStore(__dirname + '/gitwatch/gitwatch.db'));
 
 function Branches (db) {
-    this.watch = function (channel, key, cb) {
-        var where = Hash.zip(['user','repo','branch'], key.split('/'));
+    this.watch = function (channel, repo, cb) {
+        var where = Hash.zip(['user','repo','branch'], repo.match(/(\w+)/g));
+        if (!where.branch) where.branch = 'master';
+        var key = Hash(where).values.join('/');
+        
         db.get(key, function (err, branch, meta) {
-            db.save(key, Hash.merge(branch || {}, {
-                channels : (branch.channels || []).concat([ channel ]),
+            var b = branch || {};
+            var updated = Hash.merge(b, {
+                channels : (b.channels || []).concat([ channel ]),
                 user : where.user,
                 repo : where.repo,
                 name : where.name,
                 key : key,
-            }));
+            });
+            db.save(key, updated, cb);
+        });
+    };
+    
+    this.unwatch = function (channel, repo, cb) {
+        var where = Hash.zip(['user','repo','branch'], repo.match(/(\w+)/g));
+        if (!where.branch) where.branch = 'master';
+        var key = Hash(where).values.join('/');
+        
+        db.get(key, function (err, branch, meta) {
+            if (err) { if (cb) cb(err); return }
+            
+            var i = branch.channels.indexOf(channel);
+            if (i >= 0) branch.channels.splice(i,1);
+            
+            if (branch.channels.length) {
+                db.save(key, branch, cb);
+            }
+            else {
+                db.remove(key, cb);
+            }
         });
     };
     
@@ -93,6 +73,47 @@ function Branches (db) {
             }
         });
     };
+    
+    this.listen = function (cb) { 
+        setInterval(function () {
+            var hacktivity = {};
+            branches.getUpdated(function (err, branch, commit) {
+                if (err) { console.log('Error: %s', err); return }
+                
+                if (!hactivity[branch.key]) {
+                    hacktivity[branch.key] = { branch : branch, commits : [] };
+                }
+                hacktivity[key].commits.push(commit);
+            });
+            
+            Hash(hacktivity).forEach(function (repo) {
+                repo.channels.forEach(function (channel) {
+                    prepareMessage(repo, function (msg) {
+                        cb(channel, msg);
+                    });
+                });
+            });
+        }, 30000);
+    };
+}
+
+function prepareMessage (repo, cb) {
+    var greetz = ["Whoa Nelly!", "Zounds!", "Egads!", "Oh snap!", "Aack!"];
+    var greet = greetz[ Math.floor(Math.random() * greetz.length) ];
+    
+    var b = repo.branch;
+    var name = b.user + '/' + b.repo + '/ (' + b.name + ')';
+    
+    var commits = repo.commits;
+    
+    cb(greet + ' ' + commits.length + ' new commits to ' + name + '!');
+    
+    commits.slice(0,4).forEach(function (commit) {
+        cb('    * ' + commit.author.name + ': ' + commit.message);
+    });
+    
+    var more = commits.length - 4;
+    if (commits.length > 4) cb('    ... and ' + more + ' more!');
 }
 
 function takeWhile(xs, f) {
