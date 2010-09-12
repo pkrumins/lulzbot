b9fe0048a9bc90d60c4d402a20d97c3a95ea75a8
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-11 22:12:53 -0800

    logging added to ddg

--- plugins/ddg.js
+++ plugins/ddg.js
@@ -9,10 +9,10 @@ module.exports = function(search, cb) {
     var client = http.createClient(80, 'duckduckgo.com');
     var query = querystring.stringify({q: search, o: 'x'});
 
-    var request = client.request('POST', '?'+query, {host: 'duckduckgo.com'});
+    console.log(query);
+
+    var request = client.request('GET', '?'+query, {host: 'duckduckgo.com'});
     request.on('response', function (response) {
-        //console.log('STATUS: ' + response.statusCode);
-        //console.log('HEADERS: ' + JSON.stringify(response.headers));
         response.on('data', function (chunk) {
             cb(chunk);
         });
