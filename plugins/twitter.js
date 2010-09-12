6e75a19a503db468fe99b97117c396065dde8956
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-11 23:11:24 -0800

    I think this will completely work now

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,19 +1,19 @@
 var TwitterStream = require('evented-twitter').TwitterStream;
 var sys = require('sys');
 
-module.exports = function(user, pass) {
+module.exports = function(user, pass, cb) {
 
     var twitStream = new TwitterStream(user, pass);
 
     //Should make tracking available like gitwatch
-    var stream = twitstream.filter('json', {track: 'stackvm'});
+    var stream = twitStream.filter('json', {track: 'stackvm'});
 
     stream.on('ready', function() {
         stream.on('tweet', function(jsons) {
             //Checks for blank returns from twitter
             if(!String(jsons).trim()) return;
             try {
-                handleTweet(JSON.parse(jsons);
+                handleTweet(JSON.parse(jsons));
             } catch(e) {
                 sys.debug('\nProblem parsing: ' + tweet);
                 sys.debug(e.message);
@@ -35,8 +35,7 @@ module.exports = function(user, pass) {
 }
 
 function handleTweet(tweet) {
-    console.log('TWEET: '+tweet.text);
-    console.log('location: '+tweet.user.location);
-    console.log('tweet-id: '+tweet.id);
+    cb('\'\'< ♪: '+tweet.text+' -- '+tweet.screen_name+' ('+tweet.name+') \'\'< ♪');
+    cb('Czech it out @ http://twitter.com/'+tweet.screen_name+'/status/'+tweet.id);
 
 }
