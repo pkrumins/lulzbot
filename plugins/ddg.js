a6222a06104ece1c11b033d3ef3a5815024ee8fb
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-11 20:24:50 -0800

    ddg.js now reflects methods of lns.js

--- plugins/ddg.js
+++ plugins/ddg.js
@@ -1,14 +1,21 @@
 var http = require('http');
 var querystring = require('querystring');
+var sys = require('sys');
 
-var lns = http.createClient(80, 'ln-s.net');
-var query = ddg.request('GET', '/', {host: 'ln-s.net'});
-query.write(querystring.stringify({q: 'techcrunch', o: 'x'}));
-query.on('response', function (response) {
-    console.log('STATUS: ' + response.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(response.headers));
-    response.on('data', function (chunk) {
-        console.log('BODY: '+ chunk);
+//http://duckduckgo.com/?q=techcrunch&o=x
+
+module.exports = function(search, cb) {
+
+    var client = http.createClient(80, 'duckduckgo.com');
+    var query = querystring.stringify({q: query, o: 'x'});
+
+    var request = client.request('POST', '?'+query, {host: 'duckduckgo.com'});
+    request.on('response', function (response) {
+        //console.log('STATUS: ' + response.statusCode);
+        //console.log('HEADERS: ' + JSON.stringify(response.headers));
+        response.on('data', function (chunk) {
+            cb(chunk.toString('utf-8'));
+        });
     });
-});
-query.end();
+    request.end();
+}
